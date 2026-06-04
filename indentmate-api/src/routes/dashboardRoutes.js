362eb16261012c68b2dd54faec73c90103d64d12
Hemanth1372 <hemanthguntuku18@gmail.com>
2026-06-04 21:08:22 +0530

    Initial commit to new repository

--- indentmate-api/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getDashboardStats } from '../controllers/dashboardController.js'
-import { verifySuperAdmin } from '../middleware/authAdmin.js'
-
-export const dashboardRoutes = Router()
-
-dashboardRoutes.use(verifySuperAdmin)
-dashboardRoutes.get('/stats', getDashboardStats)
--- indentmate-api/src/routes/warehouseRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { listWarehouseOptions } from '../controllers/warehouseController.js'
-import { verifyToken } from '../middleware/verifyToken.js'
-
-export const warehouseRoutes = Router()
-
-warehouseRoutes.get('/options', verifyToken, listWarehouseOptions)
